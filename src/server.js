e0b6e498b86cb0ecd108670e122cd0f8199fa87b
HassanMostafaa <hassan.bmw2251@gmail.com>
2022-04-18 22:45:03 +0200

    renderHTML

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
-
+import path from "path";
+import { fileURLToPath } from "url";
 import { getAllNames } from "./controllers/nameControllers.js";
 import {
   addNewUser,
@@ -8,15 +9,23 @@ import {
   findUser,
 } from "./controllers/usersControllers.js";
 
+// 👇️ "/home/john/Desktop/javascript"
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
+
 app.use(express.json());
 app.get("/names", getAllNames);
 app.get("/", (req, res) => {
-  res.send("Landing Page");
+  res.sendFile(path.join(__dirname, "../views/index.html"));
 });
-app.get("/users", getAllUsers);
-app.post("/users", addNewUser);
-app.delete("/users/:id", deleteUser);
-app.get("/users/:id", findUser);
+app.get("/users", (req, res) => {
+  res.sendFile(path.join(__dirname, "../views/users.html"));
+});
+app.get("/api/users", getAllUsers);
+app.post("/api/users", addNewUser);
+app.delete("/api/users/:id", deleteUser);
+app.get("/api/users/:id", findUser);
 
 export default app;
