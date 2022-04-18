d66c56395bad3f9b8db44f07781ebcb21f59402b
Hassan Mostafa <48831222+HassanMostafaa@users.noreply.github.com>
2022-04-18 22:02:51 +0200

    security problem

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3001;
 
 mongoose
   .connect(
-    "mongodb://hassan:hassan123456@nodedbtest-shard-00-00.kvyza.mongodb.net:27017,nodedbtest-shard-00-01.kvyza.mongodb.net:27017,nodedbtest-shard-00-02.kvyza.mongodb.net:27017/nodeDbTest?ssl=true&replicaSet=atlas-vn74n3-shard-0&authSource=admin&retryWrites=true&w=majority",
+    "mongodb Connect String",
     { useNewUrlParser: true }
   )
   .then(() => {
